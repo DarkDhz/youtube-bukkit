ba6184f455440b2b9e495873b4ef7be52b09c91c
Arnau Gris Garcia <31670015+DarkDhz@users.noreply.github.com>
2020-04-23 16:29:31 +0200

    Add files via upload

---
+++ episodio1/serie/src/main/java/mc/virtualplanet/es/serie/Main.java
@@ -0,0 +1,17 @@
+package mc.virtualplanet.es.serie;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        System.out.println("Esto es una prueba para youtube!");
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
