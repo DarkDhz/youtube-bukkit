b9dc537a3fc5a8389c7b11e2670d48363ba4d4af
Arnau Gris Garcia <31670015+DarkDhz@users.noreply.github.com>
2020-04-24 12:17:24 +0200

    Update Test.java

--- episodio3/serie/src/main/java/mc/virtualplanet/es/serie/command/Test.java
+++ episodio3/serie/src/main/java/mc/virtualplanet/es/serie/command/Test.java
@@ -1,14 +1,11 @@
 package mc.virtualplanet.es.serie.command;
 
-import mc.virtualplanet.es.serie.items.HotbarItems;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class Test implements CommandExecutor {
-
-    private static final HotbarItems items = HotbarItems.getInstance();
 
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
 
@@ -34,10 +31,6 @@ public class Test implements CommandExecutor {
             }
             if (args[0].equalsIgnoreCase("adios")) {
                 launcher.sendMessage("Ya te vas? quedate un ratito porfa");
-                return;
-            }
-            if (args[0].equalsIgnoreCase("item")) {
-                launcher.getInventory().setItem(4, items.getBook());
                 return;
             }
         }
